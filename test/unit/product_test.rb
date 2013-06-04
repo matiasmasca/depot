a6c2c3d0cc104548e6f8fcb3a8011ffd8df62645
matiasmasca <matiasmasca@gmail.com>
2013-06-04 20:47:54 -0300

    Validaciones! facilito

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,77 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
+
+  test "product attributes must not be empty" do
+		product = Product.new
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any?
+		assert product.errors[:price].any?
+		assert product.errors[:image_url].any?
+	end
+
+  test "product price must be positive" do
+   	product = Product.new(:title       => "My Book Title",
+                          :description => "yyy",
+                          :image_url   => "zzz.jpg")
+    product.price = -1 #Precio Negativo
+    assert product.invalid?
+
+    assert_equal "must be greater than or equal to 0.01", 
+      product.errors[:price].join('; ')
+
+    product.price = 0 #Precio igual a 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", 
+      product.errors[:price].join('; ')
+
+    product.price = 1 #Precio valido.
+    assert product.valid? #assert pasa solo si es True su parametro
+  end
+
+
+  def new_product(image_url)
+    Product.new(:title       => "My Book Title",
+                :description => "yyy",
+                :price       => 1,
+                :image_url   => image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  fixtures :products #Carga el Fixture en este punto, pero Rails lo hace por defecto.
+
+  test "product is not valid without a unique title" do
+    product = Product.new(:title       => products(:ruby).title,
+                          :description => "yyy", 
+                          :price       => 1, 
+                          :image_url   => "fred.gif")
+
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(:title       => products(:ruby).title,
+                          :description => "yyy", 
+                          :price       => 1, 
+                          :image_url   => "fred.gif")
+
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+                 product.errors[:title].join('; ')
+  end
+ end
