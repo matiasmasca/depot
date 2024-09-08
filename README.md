86a25e7e52b36000e9b64af45786ea764ca38827
Matias Mascazzini <matiasmasca@gmail.com>
2024-09-07 21:23:00 -0300

    Iteration B2

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # README
+This is just the implementation of the code of the book Agile Web Development with Rails 7.2 that I have the opportunity to purchased after I had read a ilegal copy some years ago with Rails 3.2
+
+- Rails version: 7.2.1
+- Rack version: 3.1.7
+- Ruby version: ruby 3.3.2 (2024-05-30 revision e5a195edf6) +YJIT [x86_64-linux]
+
+
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "my book title",
+                          description: "yyy",
+                          image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "my book title",
+                description: "yyy",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image_url" do
+    ok =  %w[ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg https://a.b.c/x/y/z/fred.gif ]
+    bad = %w[ fred.doc fred.gif/more fred.gif.more ]
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+        "#{image_url} mustt be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+        "#{image_url} must be invalid"
+    end
+  end
 end
