da3ebe3e85e99230ab794230698a3f282eb16690
Matias Mascazzini <matiasmasca@gmail.com>
2024-09-08 16:02:09 -0300

    Iteration C2: Adding a Page Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "store#index", as: "store_index"
+
+  get "store/index"
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
