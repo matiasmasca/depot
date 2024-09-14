975c03f19393e7d45f26d0f43ee30eea7d230669
Matias Mascazzini <matiasmasca@gmail.com>
2024-09-14 14:08:59 -0300

    Cable channels working. Start chapter 12.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,10 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+
+        # Note: the params are  the Channel and the Partial
+        @product.broadcast_replace_later_to "products",
+          codpartial: "store/product"
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
