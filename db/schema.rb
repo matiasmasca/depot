b3331862e079d67706fbfdd425c7f9c7f48edf8a
Matias Mascazzini <matiasmasca@gmail.com>
2024-09-21 08:38:16 -0300

    Chp 14. Adding Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_14_170541) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_21_112226) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_14_170541) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
